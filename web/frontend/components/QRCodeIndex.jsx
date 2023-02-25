01d1b5b67ea4180e615b0ab486d55cc895990ecf
taricatarica <brunotarica@gmail.com>
2023-02-25 15:01:51 -0300

    Updated polaris dependency and improvements on question list component

--- web/frontend/components/QRCodeIndex.jsx
+++ web/frontend/components/QRCodeIndex.jsx
@@ -1,144 +1,69 @@
 import { useNavigate } from "@shopify/app-bridge-react";
 import {
-  Card,
-  Icon,
-  IndexTable,
-  Stack,
-  TextStyle,
-  Thumbnail,
-  UnstyledLink,
+  LegacyCard,
+  ResourceList,
+  ResourceItem, 
+  Text, 
+  Badge, 
+  Columns,
+  Icon
 } from "@shopify/polaris";
-import { DiamondAlertMajor, ImageMajor } from "@shopify/polaris-icons";
-
-/* useMedia is used to support multiple screen sizes */
-import { useMedia } from "@shopify/react-hooks";
-
-/* dayjs is used to capture and format the date a QR code was created or modified */
-import dayjs from "dayjs";
-
-/* Markup for small screen sizes (mobile) */
-function SmallScreenCard({
-  id,
-  title,
-  product,
-  discountCode,
-  scans,
-  createdAt,
-  navigate,
-}) {
-  return (
-    <UnstyledLink onClick={() => navigate(`/questions/${id}`)}>
-      <div
-        style={{ padding: "0.75rem 1rem", borderBottom: "1px solid #E1E3E5" }}
-      >
-        <Stack>
-          <Stack.Item>
-            <Thumbnail
-              source={product?.images?.edges[0]?.node?.url || ImageMajor}
-              alt="placeholder"
-              color="base"
-              size="small"
-            />
-          </Stack.Item>
-          <Stack.Item fill>
-            <Stack vertical={true}>
-              <Stack.Item>
-                <p>
-                  <TextStyle variation="strong">
-                    {truncate(title, 35)}
-                  </TextStyle>
-                </p>
-                <p>{truncate(product?.title, 35)}</p>
-                <p>{dayjs(createdAt).format("MMMM D, YYYY")}</p>
-              </Stack.Item>
-              <div style={{ display: "flex" }}>
-                <div style={{ flex: "3" }}>
-                  <TextStyle variation="subdued">Discount</TextStyle>
-                  <p>{discountCode || "-"}</p>
-                </div>
-                <div style={{ flex: "2" }}>
-                  <TextStyle variation="subdued">Scans</TextStyle>
-                  <p>{scans}</p>
-                </div>
-              </div>
-            </Stack>
-          </Stack.Item>
-        </Stack>
-      </div>
-    </UnstyledLink>
-  );
-}
+import { QuestionMarkMajor } from "@shopify/polaris-icons";
 
 export function QRCodeIndex({ questions, loading }) {
   const navigate = useNavigate();
-
-  /* Check if screen is small */
-  const isSmallScreen = useMedia("(max-width: 640px)");
-
-  /* Map over QRCodes for small screen */
-  const smallScreenMarkup = questions.map((question) => (
-    <SmallScreenCard key={question.id} navigate={navigate} {...question} />
-  ));
 
   const resourceName = {
     singular: "Question",
     plural: "Questions",
   };
 
-  const rowMarkup = questions.map(
-    ({ id, question, questionedBy, questionedOn, answer, answeredBy, answeredOn, productId, createdAt }, index) => {
-      const deletedProduct = false; //MUST DELETE THIS
-
-      /* The form layout, created using Polaris components. Includes the QR code data set above. */
-      return (
-        <IndexTable.Row
-          id={id}
-          key={id}
-          position={index}
-          onClick={() => {
-            navigate(`/questions/${id}`);
-          }}
-        >
-          <IndexTable.Cell>
-            <UnstyledLink data-primary-link url={`/questions/${id}`}>
-              {truncate(question, 25)}
-            </UnstyledLink>
-          </IndexTable.Cell>
-          <IndexTable.Cell>{questionedBy}</IndexTable.Cell>
-          <IndexTable.Cell>{questionedOn}</IndexTable.Cell>
-          <IndexTable.Cell>{answer}</IndexTable.Cell>
-          <IndexTable.Cell>{answeredBy}</IndexTable.Cell>
-          <IndexTable.Cell>{answeredOn}</IndexTable.Cell>
-        </IndexTable.Row>
-      );
-    }
-  );
-
-  /* A layout for small screens, built using Polaris components */
   return (
-    <Card>
-      {isSmallScreen ? (
-        smallScreenMarkup
-      ) : (
-        <IndexTable
-          resourceName={resourceName}
-          itemCount={questions.length}
-          headings={[
-            { title: "Question" },
-            { title: "Questioned By" },
-            { title: "Questioned On" },
-            { title: "Answer" },
-            { title: "Answered By" },
-            { title: "Answered On" },
-          ]}
-          selectable={false}
-          loading={loading}
-        >
-          {rowMarkup}
-        </IndexTable>
-      )}
-    </Card>
-  );
+    <LegacyCard 
+      selectable={false}
+      loading={loading}
+      >
+          <ResourceList
+        resourceName={resourceName}
+        items={questions}
+        renderItem={(item) => {
+          const {id, question, questionedBy, questionedOn, answer, answeredBy, answeredOn, productId, createdAt} = item;
+          const media = <Icon source={QuestionMarkMajor} color="highlight"  />;
+
+          return (
+            <ResourceItem
+            id={id}
+            accessibilityLabel={`View details for ${question}`}
+            media={media}
+            onClick={() => {
+              navigate(`/questions/${id}`);
+            }}
+          >
+            <Columns columns={{
+              xs: '3fr .5fr',
+              md: '7fr .5fr',
+            }}
+            gap={{
+              xs: '0',
+              md: '1',
+            }}>
+              <Text variant="bodyMd" fontWeight="bold" as="h3">
+              {truncate(question, 25)}
+              </Text>
+              <Badge progress="incomplete" status="attention">
+                Unanswered
+              </Badge>
+              <div>
+                <p>{questionedBy}</p>
+                <p>{questionedOn}</p>
+              </div>
+            </Columns>
+            </ResourceItem>
+          );
+        }}
+      />
+    </LegacyCard>
+  ); 
 }
 
 /* A function to truncate long strings */
